79d9847dc4950531020a0da1639ecf5c6fc144a9
Ted Benson <eob@csail.mit.edu>
2012-12-06 20:38:23 -0500

    edits

---
+++ santa/game.php
@@ -0,0 +1,5 @@
+<h1>Ho, ho, ho!</h1>
+
+<p>We're still in the middle of signups, <span style="color: red;"><? echo memberNick() ?></span>.</p>
+
+<p>Tell your friends to sign up.</p>
--- santa/signup.php
+++ santa/signup.php
@@ -6,9 +6,13 @@ My holiday present database crashed! I dont' know what to get anyone in CSAIL! <
 <br />
 Can you purchase a gift for a labmate for me? But there's a catch, you have to remain anonymous.</br />
 <br />
-<br />
-<form action="do_signup.php" method="POST">
+<form action="do_signup.php" method="POST" style="border: 1px solid white; background-color: #222; padding: 10px;">
+   <p>Yes! I will participate in <b style="color: red">CSAIL Secret Santa</b></p>
     My (anonymized) elf name: <input type="text" name="name" style="font-size:1.2em" />
     <input class="inputbutton" value="Help Santa" type="submit" />
 </form>
 
+<div style="margin-top: 20px; border-top: 1px solid white; color: white; font-family: Arial; font-size: 0.8em;">
+<p>Note for international students:</p>
+<p>"Secret Santa" is a holiday tradition where people get paired with someone else, and they <i>anonymously</i> purchase a small gift for that person</p>
+</div>
