d9c7e5f66d0223f6e17fdb6230387e8e9bc88267
Ted Benson <eob@csail.mit.edu>
2012-12-06 19:49:08 -0500

    htaccess

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
 $db_server   = "mysql.csail.mit.edu";
-$db_name     = "YOUR_DBNAME_HERE";
-$db_user     = "YOUR_USERNAME_HERE";
-$db_password = "YOUR_PASSWORD_HERE";
+$db_name     = "eob_santa";
+$db_user     = "santa";
+$db_password = "hoh0ho!";
 
 // This can be "assassins" or "santa"
-$game_mode = "assassins";
+$game_mode = "santa";
 
 ?>
